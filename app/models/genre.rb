512bdbea862e7126d8b38f2451cfb7a1288af732
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-15 14:54:18 -0700

    the ugliest line i've code i've ever written exists inside self.most_popular

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,9 @@
 class Genre < ActiveRecord::Base
   has_many :movie_genres
   has_many :movies, through: :movie_genres
+
+  def self.most_popular
+    MovieGenre.group(:movie_id).count.sort_by {|k, v| -v}.to_a.slice(0, 3).to_h.map{|k,v| Genre.find(k).name}
+  end
+
 end
