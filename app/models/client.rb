ba25aeaa8a1585a27072f48e54212ba4de6e6b43
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-15 09:48:23 -0700

    built a first rental method

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,14 @@
 class Client < ActiveRecord::Base
   has_many :rentals
   has_many :vhs, through: :rentals
+
+  def self.first_rental name, home_address, movie_name
+    renter = Client.new name: name, home_address: home_address
+    renter.save
+    wanted_movie = Movie.find_by title: movie_name
+    tape = Vhs.find_by movie_id: wanted_movie.id
+    new_rental = Rental.new current: true, client_id: renter.id, vhs_id: tape.id
+    new_rental.save
+  end
+
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,9 @@
 class Rental < ActiveRecord::Base
   belongs_to :client
   belongs_to :vhs
+
+  def due_date
+    
+  end
+
 end
