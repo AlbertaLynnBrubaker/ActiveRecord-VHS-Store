a1c7310a771e6aba731aa260eb673d324fe11270
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-16 09:43:07 -0700

    first!

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,8 @@ class Genre < ActiveRecord::Base
   has_many :movies, through: :movie_genres
 
   def self.most_popular
-    MovieGenre.group(:movie_id).count.sort_by {|k, v| -v}.to_a.slice(0, 3).to_h.map{|k,v| Genre.find(k).name}
+    MovieGenre.group(:movie_id).count.sort_by{|k, v| -v}.first(3).map{|k,v| Genre.find(k).name}
+    # MovieGenre.group(:movie_id).count.sort_by{|k, v| -v}.first(3).map {|genre| Genre.find(genre[1]).name}
   end
 
 end
